7e85565884557f46269b7c44cdca3f66eec4a5e3
zephyroe <114773939+zroe1@users.noreply.github.com>
2025-07-15 00:57:44 -0500

    update imports for python package

--- xlab-python-package/xlab/tests/__init__.py
+++ xlab-python-package/xlab/tests/__init__.py
@@ -2,4 +2,4 @@
 This module makes the test sections available under the `xlab.tests` namespace.
 """
 
-from . import section1_0, section1_1, section2_1_2, section2_3, robustbench, gcg
+from . import section1_0, pgd, section2_1_2, section2_3, robustbench, gcg
