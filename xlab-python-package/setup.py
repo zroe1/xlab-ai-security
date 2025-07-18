a2fe26e22bfcdb4ffa601fe74afc475b098bda5c
zephyroe <114773939+zroe1@users.noreply.github.com>
2025-07-18 12:59:26 -0500

    config python package for release

--- xlab-python-package/setup.py
+++ xlab-python-package/setup.py
@@ -12,7 +12,7 @@ def get_version():
         for line in f:
             if line.startswith('__version__'):
                 return line.split('=')[1].strip().strip('"\'')
-    return "0.1.0"
+    return "0.1.8"
 
 # Read the README file for long description
 def get_long_description():
