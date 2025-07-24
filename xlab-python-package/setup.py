63031437209e9a07606dc7b5de63616be8219301
zephyroe <114773939+zroe1@users.noreply.github.com>
2025-07-24 17:11:08 -0500

    version number for setup.py

--- xlab-python-package/setup.py
+++ xlab-python-package/setup.py
@@ -12,7 +12,7 @@ def get_version():
         for line in f:
             if line.startswith('__version__'):
                 return line.split('=')[1].strip().strip('"\'')
-    return "0.1.8"
+    return "0.1.9"
 
 # Read the README file for long description
 def get_long_description():
